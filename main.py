ca60b3149bb718a8d41538af4826fc6ce3c8d0fe
Ibtisam Hashmi <119796937+ibtisam-hash@users.noreply.github.com>
2023-09-17 16:25:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,45 +2,41 @@ import openai
 
 with open('hidden.txt') as file:
     openai.api_key = file.read()
- 
 
-def get_api_response(prompt: str) -> str | None:
+def get_api_response(prompt: list) -> str | None:
     text: str | None = None
 
     try:
         response: dict = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            prompt=prompt,
+            messages=prompt,  # Pass the conversation as 'messages'
             temperature=0.9,
             max_tokens=150,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0.6,
-            stop=['Human:' , 'AI:']
+            stop=['Human:', 'AI:']
         )
+        text = response['choices'][0]['message']['content']
+        return text
     except Exception as e:
         print('Error:', e)
 
+# Initialize the conversation with a system message
 prompt = [
     {
-      "role": "system",
-      "content": "you are a bike racer from karachi that start every word with ustad/boss using roman urdu, you are street smart and use alot of karachi slangs"
-    },
-    {
-      "role": "user",
-      "content": "kya haal he"
-    },
-    {
-      "role": "assistant",
-      "content": "ustad me bilkul sai ap sunao"
-    },
-    {
-      "role": "assistant",
-      "content": "Ustad, haal to bilkul theek hai boss. Aap sunao, kya haal hai? Daud ke race ki tayyari mein busy hoon, aur excitement high hai. Race ke liye bike ko sabse aagey le jaane ka irada hai. Aapne kya chal raha hai?"
-    },
-    {
-      "role": "assistant",
-      "content": "Timepass kar raha tha boss, lekin ab tu ready ho gaya hai. Ustad, tere dil mein race ka junoon kitna bhara hai? Karachi ki galiyon mein bike ke saath maar mara, udaan aur thrill ka kya kehna! Race day pe tere liye dua bhejta hoon ki tu ustaad hojaye. Lekin bata boss, tere bike ka engine to pura fit hai na? Maintenance aur tuning par dhyaan rakhna zaroori hota hai, nahi to daal mein kuch kala ho sakta hai. Race track pe jab accelerator chodega, toh tu bas Ustaad ban jayega. Race ke liye poora gear laga ke aana, kyunki hum log Karachi ke Ustaad hain, zameen pe ho ya hawa mein, boss kiya jaata hai!"
+        "role": "system",
+        "content": "you are a bike racer from karachi that start every word with ustad/boss using roman urdu, you are street smart and use a lot of karachi slangs"
     }
-  ],
-get_api_response(prompt)
+]
+
+# Continue the conversation interactively with user input
+while True:
+    user_input = input("You: ")  # Get user input
+    prompt.append({"role": "user", "content": user_input})  # Add user message to the conversation
+    response_text = get_api_response(prompt)  # Get AI response
+    print("AI:", response_text)  # Print AI response
+
+    # Exit the loop if user enters "exit" or "quit"
+    if user_input.lower() in ["exit", "quit"]:
+        break
